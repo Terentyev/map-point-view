98a5e6b4e344d77b47b9bd9e6334193940ffe037
Alexander Terentyev <gyran@bk.ru>
2012-10-31 23:29:20 +1100

    Support file read for IE and Mozilla

--- scripts/map-point-view-ui.js
+++ scripts/map-point-view-ui.js
@@ -1,21 +1,55 @@
 /**
  * Open file and read it.
  */
-function MapPointViewUI_load_data(file, form) {
-  if (!file) {
-    return;
-  }
-
-  var reader = new FileReader();
-
-  reader.onload = function(e) {
-    if (e.target.readyState == FileReader.DONE) {
-      var data = MapPointViewLoadData(file, e.target.result);
-      MapPointViewUI_select_fields(data, form);
+function MapPointViewUI_load_data(file_input, form) {
+  if (/*@cc_on!@*/0) {
+    // is MSIE
+    var files = document.getElementById(file_input).files;
+    if (files.length != 0) {
+      alert('Should select one file');
+      return;
     }
-  };
 
-  reader.readAsBinaryString(file.slice(0, file.size));
+    try {
+      var fso = new ActiveXObject("Scripting.FileSystemObject");
+      var file = fso.OpenTextFile(files[0].file, 1);
+      var fileContent = file.ReadAll();
+       
+      file.Close();
+       
+      return fileContent;
+    } catch (e) {
+      if (e.number == -2146827859) {
+        alert('Unable to access local files due to browser security settings. ' +
+        'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
+        'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
+      }
+    }
+  }
+  else {
+    var file = $('#' + file_input).prop('files')[0];
+    if (!file) {
+      return;
+    }
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      if (e.target.readyState == FileReader.DONE) {
+        var data = MapPointViewLoadData(file, e.target.result);
+        MapPointViewUI_select_fields(data, form);
+      }
+    };
+
+    var slice = null;
+    if (jQuery.browser.mozilla) {
+      slice = file.mozSlice(0, file.size);
+    }
+    else {
+      slice = file.slice(0, file.size);
+    }
+    reader.readAsBinaryString(slice);
+  }
 }
 
 /**
